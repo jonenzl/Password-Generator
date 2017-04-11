f1a1fa2b2c7e91ed322afe93a9345d31d35380bb
Jonathan Ellis <thejonathanellis@gmail.com>
2017-04-11 16:42:08 +1200

    Changed using statements at top of .cs file.

--- Password-Generator/MainWindow.xaml.cs
+++ Password-Generator/MainWindow.xaml.cs
@@ -1,17 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Password_Generator
 {
@@ -85,7 +74,7 @@ namespace Password_Generator
             try
             {
                 // Password length
-                int length = Convert.ToInt32(comboBox.Text);
+                int length = Convert.ToInt32(sliderText.Text);
 
                 // Generate password
                 string pass = generatePassword(length);
